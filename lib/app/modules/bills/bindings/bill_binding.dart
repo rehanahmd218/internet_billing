4169fdab7f93b9ccd04682736092efe14d0e43b9
Rehan Ahmed <rehanahmd218@gmail.com>
2025-12-24 01:37:21 +0500

    sdfdsf

--- lib/app/modules/bills/bindings/bill_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:wifi_billing/app/modules/bills/controllers/bill_controller.dart';
-
-class BillBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<BillController>(() => BillController());
-  }
-}
--- lib/app/modules/users/bindings/user_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:wifi_billing/app/modules/users/controllers/user_controller.dart';
-
-class UserBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<UserController>(() => UserController());
-  }
-}
